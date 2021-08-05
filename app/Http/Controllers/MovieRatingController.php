4b94c52dd6526dee6bd923c4c74c69944a2b6f1d
alessandroTola <alessandro.tola@yahoo.it>
2021-08-05 09:34:27 +0200

    Create files for MovieRating
    -Model: MovieRating
    -Controller: MovieRatingController
    -Resources: MovieRatingResource , MovieRatingCollection
    -Factories: MovieRatingFactory
    -Seeder: MovieRatingSeeder
    -Migration: 2021_08_05_073149_create_movie_ratings_table

---
+++ app/Http/Controllers/MovieRatingController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MovieRating;
+use Illuminate\Http\Request;
+
+class MovieRatingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MovieRating  $movieRating
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MovieRating $movieRating)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MovieRating  $movieRating
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MovieRating $movieRating)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MovieRating  $movieRating
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MovieRating $movieRating)
+    {
+        //
+    }
+}
---
+++ database/factories/MovieRatingFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\MovieRating;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MovieRatingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = MovieRating::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
